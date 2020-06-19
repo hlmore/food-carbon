bce3ad3b0943dfc5f8a248e7cc602c7f13a9487d
Heather_sumac <hmore@sfu.ca>
2020-06-19 13:52:59 -0700

    Initial commit of data and code to draft figs

---
+++ FoodCarbon_plots.R
@@ -0,0 +1,119 @@
+# This code reads data on food carbon emissions, and creates plots.
+#
+# It will hopefully be integrated into a Shiny app at some point.
+#
+# Started by Heather More on 2020-06-17
+#
+# Data from:
+#    Reducing food's environmental impacts through producers and consumers
+#    Table S2
+#    J. Poore, T. Nemecek
+#    Science 2018-06-01: 987-992
+
+#' <!-- ==================================================================== -->
+#' 
+#' ## Load required packages
+library(readxl)     # read Excel spreadsheets
+library(dplyr)      # data manipulation
+library(tidyr)      # tidying data
+library(ggplot2)    # nice plots
+library(ggrepel)    # separate labels of points in scatterplot
+
+#' <!-- ==================================================================== -->
+#' 
+#' File information.  On Windows, change \ in file paths to / or \\
+
+filePath  <- "D:/Documents/Code/Food carbon"
+fileName  <- "Poore_2018_DataS2_tidy.xls"
+dataSheetName <- "GlobalTotals_tidy"  # data
+metaSheetName <- "GlobalTotals_meta"  # metadata
+colourSheetName <- "Type_meta"  # colours
+
+#' <!-- ==================================================================== -->
+#' 
+#' ## Read in data and metadata
+
+df <- read_excel(file.path(filePath, fileName, 
+                           fsep = .Platform$file.sep), sheet = dataSheetName)
+df_meta <- read_excel(file.path(filePath, fileName, 
+                           fsep = .Platform$file.sep), sheet = metaSheetName)
+df_colours <- read_excel(file.path(filePath, fileName, 
+                                fsep = .Platform$file.sep), sheet = colourSheetName)
+#' Check variables
+str(df)
+str(df_meta)
+str(df_colours)
+
+#' Function to get units for a given variable name
+GetUnits <- function(string_in) {
+  varName <- strsplit(string_in, "_")[[1]][1]
+  string_out <- df_meta %>% 
+    filter(VariableName==varName) %>% 
+    select(Units) %>% 
+    toString()
+  return(string_out)
+}
+
+#' Function to get colour for a given variable name
+GetColour <- function(string_in) {
+  string_out <- df_colours %>% 
+    filter(Type==string_in) %>% 
+    select(Colour) %>% 
+    toString()
+  return(string_out)
+}
+
+#' <!-- ==================================================================== -->
+#' 
+#' ## Plot all mass vs. total greenhouse gas emissions, and label foods
+
+# Make new variable containing sum of all greenhouse gas emissions
+df$ghg_total <- df %>%  
+  select(starts_with("ghg")) %>% 
+  rowSums(., na.rm = TRUE)
+
+#' Make list of colours to use when plotting
+#' https://www.r-bloggers.com/a-detailed-guide-to-ggplot-colors/
+#' https://www.datamentor.io/r-programming/list/
+colours_type <- c()
+for (item in unique(df$type)){
+  colours_type[[item]] <- GetColour(item)
+}
+
+# Plot
+plot_mass_ghg <- ggplot(data = df) +
+  aes(x = mass, y = mass*ghg_total, colour = type) +
+  scale_x_log10() +
+  scale_y_log10() +
+  scale_color_manual(values = colours_type) +
+  geom_point(size=3, alpha=0.7) +
+  geom_point(size=3, shape=1) +
+  #geom_text(aes(label=Product),hjust=0, vjust=0) +  # label points:  https://stackoverflow.com/a/15625149
+  geom_text_repel(aes(label = Product),
+                 box.padding   = 0.35, 
+                 point.padding = 0.5,
+                 show.legend = FALSE) +  # label points with auto-repelling text:  https://stackoverflow.com/a/48762376
+  theme_light() +
+  labs(x = paste("Total food mass produced (", GetUnits("mass"), ")"),
+       y = paste("Total GHG mass produced (", GetUnits("ghg"), ")"), 
+       colour = "Type",
+       title = "Totals produced per year")
+x11()
+print(plot_mass_ghg)
+
+#' <!-- ==================================================================== -->
+#' 
+#' ## Plot greenhouse gas emissions at each stage of production
+
+df_ghg_long <- df %>% 
+  select((starts_with("ghg") & !ends_with("total"))
+         | "Product") %>% 
+  pivot_longer(-Product, names_to = "stage", values_to = "stage_ghg_emissions")
+
+# Parallel coordinates plot
+# https://datascience.blog.wzb.eu/2016/09/27/parallel-coordinate-plots-for-discrete-and-categorical-data-in-r-a-comparison/
+stages_plot <- ggplot(data = df_ghg_long) +
+  aes(x = stage, y = stage_ghg_emissions, group = Product) +   # group = Product is important!
+  geom_path(aes(color = Product))
+x11()
+print(stages_plot)
